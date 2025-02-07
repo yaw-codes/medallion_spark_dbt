ce3cec0eeedcdb1da116c18e9a2039a4edfa6c1a
Stanley Yaw Appiah <116563869+yaw-codes@users.noreply.github.com>
2025-02-07 14:18:27 +0000

    dbt databricks transformations
    using dbt to make transformations in databricks

---
+++ medallion_spark_dbt/target/compiled/medallion_spark_dbt/models/marts/sales/sales.sql
@@ -0,0 +1,104 @@
+
+
+with salesorderdetail_snapshot as (
+    SELECT
+        SalesOrderID,
+        SalesOrderDetailID,
+        OrderQty,
+        ProductID,
+        UnitPrice,
+        UnitPriceDiscount,
+        LineTotal
+    FROM `hive_metastore`.`snapshots`.`salesorderdetail_snapshot`
+),
+
+product_snapshot as (
+    SELECT
+        ProductID,
+        Name,
+        ProductNumber,
+        Color,
+        StandardCost,
+        ListPrice,
+        Size,
+        Weight,
+        SellStartDate,
+        SellEndDate,
+        DiscontinuedDate,
+        ThumbNailPhoto,
+        ThumbnailPhotoFileName
+    FROM `hive_metastore`.`saleslt`.`product`
+),
+
+saleorderheader_snapshot as (
+    SELECT
+        SalesOrderID,
+        RevisionNumber,
+        OrderDate,
+        DueDate,
+        ShipDate,
+        Status,
+        OnlineOrderFlag,
+        SalesOrderNumber,
+        PurchaseOrderNumber,
+        AccountNumber,
+        CustomerID,
+        ShipToAddressID,
+        BillToAddressID,
+        ShipMethod,
+        CreditCardApprovalCode,
+        SubTotal,
+        TaxAmt,
+        Freight,
+        TotalDue,
+        Comment,
+        row_number() over (partition by SalesOrderID order by SalesOrderID) as row_num
+    FROM `hive_metastore`.`saleslt`.`salesorderheader`
+),
+
+transformed as (
+    select
+        sod.SalesOrderID,
+        sod.SalesOrderDetailID,
+        sod.OrderQty,
+        sod.ProductID,
+        sod.UnitPrice,
+        sod.UnitPriceDiscount,
+        sod.LineTotal,
+        p.Name,
+        p.ProductNumber,
+        p.Color,
+        p.StandardCost,
+        p.ListPrice,
+        p.Size,
+        p.Weight,
+        p.SellStartDate,
+        p.SellEndDate,
+        p.DiscontinuedDate,
+        p.ThumbNailPhoto,
+        p.ThumbnailPhotoFileName,
+        soh.RevisionNumber,
+        soh.OrderDate,
+        soh.DueDate,
+        soh.ShipDate,
+        soh.Status,
+        soh.OnlineOrderFlag,
+        soh.SalesOrderNumber,
+        soh.PurchaseOrderNumber,
+        soh.AccountNumber,
+        soh.CustomerID,
+        soh.ShipToAddressID,
+        soh.BillToAddressID,
+        soh.ShipMethod,
+        soh.CreditCardApprovalCode,
+        soh.SubTotal,
+        soh.TaxAmt,
+        soh.Freight,
+        soh.TotalDue,
+        soh.Comment
+    from salesorderdetail_snapshot sod
+    left join product_snapshot p on sod.ProductID = p.ProductID
+    left join saleorderheader_snapshot soh on sod.SalesOrderID = soh.SalesOrderID
+)
+
+select * from transformed
